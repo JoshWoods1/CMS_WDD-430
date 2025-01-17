64f36e1adfc1d757a27c03ea2bcff5c9a0f5388b
JoshWoods1 <woo19047@byui.edu>
2025-01-17 15:27:42 -0700

    w02 activity completed

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  standalone: false,
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent {
+  contacts: Contact[] = [
+  new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', 'images/jacksonk.jpg', null),
+  new Contact('2', 'Rex Barzee', 'barzeer@byui.edu','208-496-3768','images/barzeer.jpg', null)]
+}
